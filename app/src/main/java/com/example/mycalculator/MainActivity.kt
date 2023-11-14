ef330460766f42ef78271459fda84c195486e875
bill chipman <wrchipman@gmail.com>
2023-11-13 22:52:56 -0500

    add calculations and results

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -53,17 +53,67 @@ class MainActivity : ComponentActivity() {
         binding.btnBackspace.setOnClickListener { handleBackspace()}
     }
 
-    private fun appendNumber(number: String) {}
-
-    private fun handleBackspace() {}
-
-    private fun setOperator(operator: Operator) {}
-
-    private fun operatorToString(operator: Operator): String {
-        return ""
+    private fun appendNumber(number: String) {
+        currentInput.append(number)
+        updateDisplay()
     }
 
-    private fun calculateResult() {}
+    private fun handleBackspace() {
+        if (currentInput.isNotEmpty()) {
+            currentInput.deleteCharAt(currentInput.length - 1)
+            updateDisplay()
+        }
+    }
+
+    private fun setOperator(operator: Operator) {
+        if (operand1 == null) {
+            operand1 = BigDecimal(currentInput.toString())
+            currentInput.clear()
+        }
+        binding.tvOldInput.text = operand1.toString()
+        binding.tvInput.text = ""
+        currentOperator = operator
+        binding.tvCurrentOperand.text = operatorToString(operator)
+        currentInput.clear()
+    }
+
+    private fun operatorToString(operator: Operator): String {
+        return when (operator) {
+            Operator.ADD -> "+"
+            Operator.SUBTRACT -> "-"
+            Operator.MULTIPLY -> "x"
+            Operator.DIVIDE -> "÷"
+            Operator.NONE -> ""
+        }
+    }
+
+    private fun calculateResult() {
+        val operand2 = BigDecimal(currentInput.toString())
+        var result: BigDecimal? = null
+
+        when (currentOperator) {
+            Operator.ADD -> result = operand1?.add(operand2)
+            Operator.SUBTRACT -> result = operand1?.subtract(operand2)
+            Operator.MULTIPLY -> result = operand1?.multiply(operand2)
+            Operator.DIVIDE -> {
+                if (operand2 != BigDecimal.ZERO)
+                    operand1?.divide(operand2, 10, RoundingMode.HALF_UP).also {result = it}
+            }
+            Operator.NONE -> result = operand2
+        }
+
+        if (result != null) {
+            var displayString =
+                String.format("$operand1%s$operand2", operatorToString(currentOperator))
+            if (operand1 == null)
+                displayString = "$operand2"
+            binding.tvOldInput.text = displayString
+            binding.tvInput.text = String.format(result.toString())
+            operand1 = result
+            currentInput.clear()
+            currentInput.append(result)
+        }
+    }
 
     private fun allClearInput() {
         currentInput.clear()
