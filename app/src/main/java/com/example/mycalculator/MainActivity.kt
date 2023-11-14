0850955e1aaa2701e9b62e39d298edfc1882a615
bill chipman <wrchipman@gmail.com>
2023-11-13 21:59:08 -0500

    clickl listeners and clear functions complete

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -2,18 +2,19 @@ package com.example.mycalculator
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycalculator.databinding.ActivityMainBinding
-import com.example.mycalculator.ui.theme.MyCalculatorTheme
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class MainActivity : ComponentActivity() {
+
+    private var currentInput = StringBuilder()
+    private var currentOperator = Operator.NONE
+    private var operand1: BigDecimal? = null
+
+    enum class Operator {
+        NONE, ADD, SUBTRACT, MULTIPLY, DIVIDE
+    }
 
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +23,66 @@ class MainActivity : ComponentActivity() {
         val view = binding.root
         setContentView(view)
 
+        //Click listeners for number buttons
+        binding.btnOne.setOnClickListener { appendNumber("1") }
+        binding.btnTwo.setOnClickListener { appendNumber("2") }
+        binding.btnThree.setOnClickListener { appendNumber("3") }
+        binding.btnFour.setOnClickListener { appendNumber("4") }
+        binding.btnFive.setOnClickListener { appendNumber("5") }
+        binding.btnSix.setOnClickListener { appendNumber("6") }
+        binding.btnSeven.setOnClickListener { appendNumber("7") }
+        binding.btnEight.setOnClickListener { appendNumber("8") }
+        binding.btnNine.setOnClickListener { appendNumber("9") }
+        binding.btnZero.setOnClickListener { appendNumber("0") }
+        binding.btnDot.setOnClickListener { appendNumber(".") }
+
+        //Click listeners for operator buttons
+        binding.btnPlus.setOnClickListener { setOperator(Operator.ADD) }
+        binding.btnMinus.setOnClickListener { setOperator(Operator.SUBTRACT) }
+        binding.btnMultiply.setOnClickListener { setOperator(Operator.MULTIPLY) }
+        binding.btnDivide.setOnClickListener { setOperator(Operator.DIVIDE) }
+
+        // Click listener for evaluation
+        binding.btnEqual.setOnClickListener { calculateResult() }
+
+        // Click listeners for clear buttons
+        binding.btnClear.setOnClickListener { clearInput() }
+        binding.btnAllClear.setOnClickListener { allClearInput() }
+
+        //Click Listener for backspace
+        binding.btnBackspace.setOnClickListener { handleBackspace()}
     }
+
+    private fun appendNumber(number: String) {}
+
+    private fun handleBackspace() {}
+
+    private fun setOperator(operator: Operator) {}
+
+    private fun operatorToString(operator: Operator): String {
+        return ""
+    }
+
+    private fun calculateResult() {}
+
+    private fun allClearInput() {
+        currentInput.clear()
+        operand1 = null
+        currentOperator = Operator.NONE
+        binding.tvOldInput.text = ""
+        binding.tvInput.text = "0"
+        binding.tvCurrentOperand.text = ""
+    }
+
+    private fun clearInput() {
+        currentInput.clear()
+        currentOperator = Operator.NONE
+        binding.tvInput.text = "0"
+        operand1 = null
+    }
+
+    private fun updateDisplay() {
+        binding.tvInput.text = currentInput.toString()
+    }
+
 }
